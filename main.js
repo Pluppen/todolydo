f2217d52b9d4fcd544c827423bd8037c97338f9e
Elias Juremalm <elias.juremalm@learnet.se>
2020-08-20 10:13:31 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,29 @@
+const {app, BrowserWindow} = require('electron');
+
+function createWindow () {
+    const win = new BrowserWindow({
+        width: 495,
+        height: 700,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    })
+
+    win.loadFile('index.html')
+
+    win.webContents.openDevTools()
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
+app.on('activate', () => {
+    if(BrowserWindow.getAllWindows().length === 0){
+        createWindow()
+    }
+})
